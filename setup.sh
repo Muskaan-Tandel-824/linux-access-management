5eeeb8183ac05400478f8dc40f88884a18d291a5
EC2 Default User <ec2-user@ip-172-31-19-3.ap-southeast-2.compute.internal>
2026-01-15 13:42:14 +0000

    linux user and access management system

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ groupadd admin
 groupadd developer
 groupadd auditor
 
-useradd -m -G admin alice
-useradd -m -G developer bob
+useradd -m -G admin alicee
+useradd -m -G developer bobbyy
 useradd -m -G auditor john
 
